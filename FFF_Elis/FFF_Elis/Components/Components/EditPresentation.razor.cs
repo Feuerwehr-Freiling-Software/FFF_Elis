20d7b29962b3cdcd844774f5ffadcf3beb7da316
HaunBa <haunschmied.b@gmail.com>
2024-09-03 18:02:17 +0200

    DRECKS SWAPY FUNKTIONIERT HWR

---
+++ FFF_Elis/FFF_Elis/Components/Components/EditPresentation.razor.cs
@@ -0,0 +1,15 @@
+﻿using FFF_Elis.Components.BusinessObjects;
+using Microsoft.AspNetCore.Components;
+
+namespace FFF_Elis.Components.Components;
+
+public partial class EditPresentation : ComponentBase
+{
+    public List<VisualItem> VisualItems { get; set; } = new List<VisualItem>();
+
+    private async Task AddNewItem()
+    {
+        VisualItems.Add(new VisualItem() { Name = "Test", Description = "test", Type = VisualItemType.Webpage, Duration = 10, Url = "benis" });
+        await InvokeAsync(StateHasChanged);
+    }
+}
---
+++ FFF_Elis/FFF_Elis/Components/Pages/VisuSettings.razor.cs
@@ -0,0 +1,24 @@
+﻿using FFF_Elis.Components.BusinessObjects;
+using Microsoft.AspNetCore.Components;
+
+namespace FFF_Elis.Components.Pages;
+
+public partial class VisuSettings : ComponentBase
+{
+    private List<VisualItem> VisualItems { get; set; } = [];
+    private int SelectedIndex { get; set; } = 0;
+
+    #region Präsentation
+
+    private void AddPresentationItem()
+    {
+        VisualItems.Add(new VisualItem { Type = VisualItemType.Webpage });
+    }
+    
+    private void RemovePresentationItem(VisualItem item)
+    {
+        VisualItems.Remove(item);
+    }
+
+    #endregion
+}
